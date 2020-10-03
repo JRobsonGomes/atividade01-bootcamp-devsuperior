4fc2e3e1e0bc2a172247e7bd81542bbf2c3f312c
Robson <robsong200@gmail.com>
2020-10-03 17:15:41 -0300

    FindById Client, exception handling

--- clients/src/main/java/com/robson/clients/services/ClientService.java
+++ clients/src/main/java/com/robson/clients/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.robson.clients.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.robson.clients.dto.ClientDTO;
 import com.robson.clients.entities.Client;
 import com.robson.clients.repositories.ClientRepository;
+import com.robson.clients.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +23,12 @@ public class ClientService {
 	public List<ClientDTO> findAll(){
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
